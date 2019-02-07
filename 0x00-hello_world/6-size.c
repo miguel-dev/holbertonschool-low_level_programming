d921029da7d43b29bea7484b4d85308bc3eef541
miguel-dev <miguelacortes@zoho.com>
2019-02-07 17:06:48 +0000

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,9 @@
 int main(void)
 {
 printf("Size of a char:%i", sizeof(char));
- printf("Size of an int:%1", sizeof(int));
- printf("Size of a long int:%1", sizeof(long int));
- printf("Size of a long long int:%1", sizeof(long long int));
- printf("Size of a float:%i", sizeof(float));
+printf("Size of an int:%1", sizeof(int));
+printf("Size of a long int:%1", sizeof(long int));
+printf("Size of a long long int:%1", sizeof(long long int));
+printf("Size of a float:%i", sizeof(float));
 return (0);
 }
