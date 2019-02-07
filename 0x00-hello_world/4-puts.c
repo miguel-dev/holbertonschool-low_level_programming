ed5533a6f562cc50cdf8cb95a1582ae9a876cb9e
miguel-dev <miguelacortes@zoho.com>
2019-02-07 15:52:10 +0000

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*Prints a string followed by a new line*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
