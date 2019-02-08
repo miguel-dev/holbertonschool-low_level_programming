b6c1c3bed5c84d51a591db570a80c82eec748e8b
miguel-dev <miguelacortes@zoho.com>
2019-02-08 00:01:43 +0000

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	number = 0;
+	while (number < 10)
+	{
+		printf("%d", number);
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
