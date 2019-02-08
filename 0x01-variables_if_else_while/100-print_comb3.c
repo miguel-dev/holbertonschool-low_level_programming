079199a5f2bc0dcf87b7a2cf761c2e37832b1b57
miguel-dev <miguelacortes@zoho.com>
2019-02-08 22:30:24 +0000

    Task 11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit1;
+	int digit2;
+
+	for (digit1 = 48; digit1 <= 56; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 <= 57; digit2++)
+		{
+			putchar(digit1);
+			putchar(digit2);
+			if (digit1 != 56 || digit2 != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
