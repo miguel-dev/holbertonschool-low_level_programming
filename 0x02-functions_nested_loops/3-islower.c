020a416d2a56cb7564f8baf0f18128fb775e63bf
miguel-dev <miguelacortes@zoho.com>
2019-02-12 02:06:11 +0000

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/*
+ * _islower - Checks for lowercase character.
+ * @c: The character to be checked to see if it's lowercase.
+ *
+ * Return: 1 if c is lowercase, 0 otherwise.
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
