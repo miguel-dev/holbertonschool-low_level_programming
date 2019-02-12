de4398ba6e8faf42617b711f167c096e849da235
miguel-dev <miguelacortes@zoho.com>
2019-02-12 20:14:53 +0000

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer.
+ */
+
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar(h / 10 + 48);
+			_putchar(h % 10 + 48);
+			_putchar(':');
+			_putchar(m / 10 + 48);
+			_putchar(m % 10 + 48);
+			_putchar('\n');
+		}
+	}
+}
