a12a52bc8615aca2404c5c3ed3c446e6d5e94ae7
miguel-dev <miguelacortes@zoho.com>
2019-02-13 00:05:53 +0000

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers and returns the result.
+ * @a: integer to be added.
+ * @b: integer to be added.
+ *
+ * Return: result of addition.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
