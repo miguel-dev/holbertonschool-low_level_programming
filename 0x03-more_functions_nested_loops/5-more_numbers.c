eb7f1742783c8ce195b12cd6bb7eaacbe811c08b
miguel-dev <miguelacortes@zoho.com>
2019-02-14 21:00:46 +0000

    Task 5

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - print 10 times the numbers from 0 to 14.
+ */
+
+void more_numbers(void)
+{
+	int t;
+	int n;
+
+	for (t = 0; t < 10; t++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n < 10)
+			{
+				_putchar(n + '0');
+			}
+			else
+			{
+				_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
