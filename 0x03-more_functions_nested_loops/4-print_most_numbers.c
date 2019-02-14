154b67251fd173be0b51f2999d99f6ead1272a75
miguel-dev <miguelacortes@zoho.com>
2019-02-14 20:14:21 +0000

    Task 4

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - print digits except 2 and 4.
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		if (n != 2 && n != 4)
+			_putchar(n + '0');
+		n++;
+	}
+	_putchar('\n');
+}
