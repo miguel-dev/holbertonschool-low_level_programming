8e3f032838d9a43cc80ee10a52ddb880e23711ac
miguel-dev <miguelacortes@zoho.com>
2019-02-15 02:57:51 +0000

    Task 9

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,16 +15,16 @@ int main(void)
 	{
 		if (n != 100)
 		{
-			if (n % 3 == 0)
-				printf("Fizz ");
+			if (n % 3 == 0 && n % 5 == 0)
+				printf("FizzBuzz ");
 			else
 			{
-				if (n % 5 == 0)
-					printf("Buzz ");
+				if (n % 3 == 0)
+					printf("Fizz ");
 				else
 				{
-					if (n % 3 == 0 && n % 5 == 0)
-						printf("FizzBuzz ");
+					if (n % 5 == 0)
+						printf("Buzz ");
 					else
 						printf("%d ", n);
 				}
