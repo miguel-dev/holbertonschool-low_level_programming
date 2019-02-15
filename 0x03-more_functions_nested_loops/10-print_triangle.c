ac118c9c5d162fc0120b3476c056ae69dd9fde35
miguel-dev <miguelacortes@zoho.com>
2019-02-15 22:43:23 +0000

    Task 10

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line.
+ * @size: size of the triangle.
+ */
+
+void print_triangle(int size)
+{
+	int r;
+	int c;
+
+	for (r = 1; r <= size; r++)
+	{
+		for (c = 0; c < size; c++)
+		{
+			if (c < size - r)
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
