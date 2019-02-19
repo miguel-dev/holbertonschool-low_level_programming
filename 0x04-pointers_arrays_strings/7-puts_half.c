dfd32bdb06785320d99d91b2fdc61e0a1f20ca89
miguel-dev <miguelacortes@zoho.com>
2019-02-19 21:50:35 +0000

    Task 7

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -9,17 +9,33 @@ void puts_half(char *str)
 {
 	int len;
 	int index;
+	int n;
 
+	len = 0;
 	while (str[len] != '\0')
 	{
 		len++;
 	}
+	len--;
 
-	index = len / 2;
-	while (index < len)
+	if (len % 2 != 0)
 	{
-		_putchar(str[index]);
-		index++;
+		n = (len - 1) / 2;
+		index = len - n;
+		while (str[index] != '\0')
+		{
+			_putchar(str[index]);
+			index++;
+		}
+	}
+	else
+	{
+		index = len / 2;
+		while (index <= len)
+		{
+			_putchar(str[index]);
+			index++;
+		}
 	}
 	_putchar('\n');
 }
