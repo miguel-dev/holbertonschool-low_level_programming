4a2ae2a5e75f5ec4d1efc1d344aeea91d2171e28
miguel-dev <miguelacortes@zoho.com>
2019-02-19 16:59:23 +0000

    Task 4

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -11,13 +11,14 @@ void print_rev(char *s)
 
 	index = 0;
 	while (s[index] != '\0')
+	{
 		index++;
-
+	}
+	index--;
 	while (index >= 0)
 	{
 		_putchar(s[index]);
 		index--;
 	}
-
 	_putchar('\n');
 }
