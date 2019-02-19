babb64682258a8bff4877d4f278247fec0a2a0e7
miguel-dev <miguelacortes@zoho.com>
2019-02-19 00:51:49 +0000

    Task 0

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as a parameter and updates
+ * the value it points to 98.
+ * @n: pointer to an int.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
