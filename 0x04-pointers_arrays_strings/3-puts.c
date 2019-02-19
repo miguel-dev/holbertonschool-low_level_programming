400d0a47610c623ddf0fdaadd575eba6320f81d5
miguel-dev <miguelacortes@zoho.com>
2019-02-19 15:16:21 +0000

    Task 3

---
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: string of characters.
+ */
+
+void _puts(char *str)
+{
+	int index;
+
+	index = 0;
+	while (str[index] != '\0')
+	{
+		_putchar(str[index]);
+		index++;
+	}
+	_putchar('\n');
+}
