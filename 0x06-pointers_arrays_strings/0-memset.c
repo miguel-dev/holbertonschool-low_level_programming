2d17c8abcac343ff61126773d220e93d5f63e9cf
miguel-dev <miguelacortes@zoho.com>
2019-02-25 20:26:51 +0000

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _memset - fills memory with a constant byte.
+ * @s: pointer to memory area to be filled.
+ * @b: constant byte.
+ * @n: number of bytes to be filled.
+ *
+ * Return: pointer to the memory area.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
