63402dfb9db5864645eb6ace91e8ef38c8b226c0
miguel-dev <miguelacortes@zoho.com>
2019-02-26 02:34:52 +0000

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - searches for a string for any of a set of bytes.
+ * @s: string to be searched.
+ * @accept: string to be compared with.
+ *
+ * Return: pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != 0)
+	{
+		j = 0;
+		while (accept[j] != 0)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
