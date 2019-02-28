459648fab255d9b67345596349104598fa920fff
miguel-dev <miguelacortes@zoho.com>
2019-02-28 21:44:50 +0000

    Task 5

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root.
+ * @n: number
+ *
+ * Return: natural square root, otherwise it returns -1
+ */
+
+int _sqrt_recursion2(int x, int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (x * x == n)
+	{
+		return (x);
+	}
+	else if (x < n)
+		return _sqrt_recursion2(x + 1, n);
+	else
+		return (-1);
+}
+
+int _sqrt_recursion(int n)
+{
+	return _sqrt_recursion2(0, n);
+}
