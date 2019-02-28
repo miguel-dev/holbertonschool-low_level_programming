2b9faa12d0265eab74df4a5ffc9df195b2238274
miguel-dev <miguelacortes@zoho.com>
2019-02-28 00:48:21 +0000

    Task 2

---
+++ 0x07-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string to be measured for length.
+ *
+ * Return: integer for length of string.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
