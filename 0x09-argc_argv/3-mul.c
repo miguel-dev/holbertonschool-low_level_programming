dbac56858081f133fbf21f974d07266fe2160fe1
miguel-dev <miguelacortes@zoho.com>
2019-03-03 00:14:44 +0000

    Task 3

---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the result of multiplication, followed by a new line.
+ * @argc: number of arguments.
+ * @argv: array of arguments.
+ *
+ * Return: 0, always.
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc - 1 == 2)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a * b);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
