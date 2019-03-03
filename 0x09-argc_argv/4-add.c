c7b51d9faebdf5b78e37be8cd45678b607bde040
miguel-dev <miguelacortes@zoho.com>
2019-03-03 02:02:44 +0000

    Task 4

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -13,10 +13,23 @@
 int main(int argc, char *argv[])
 {
 	int check_number = 1;
+	int number = 0;
 	int sum = 0;
 
 	while (check_number < argc)
 	{
+		while (argv[check_number][number] != 0)
+		{
+			if (isdigit(argv[check_number][number]) > 0)
+			{
+				number++;
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 		if (isdigit(*argv[check_number]) > 0)
 		{
 			sum += atoi(argv[check_number]);
