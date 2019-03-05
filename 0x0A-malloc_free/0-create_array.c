292a9e943fc78f496b75b11012e09d3f22b39eb4
miguel-dev <miguelacortes@zoho.com>
2019-03-05 20:12:51 +0000

    Task 0

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -1,25 +1,39 @@
 #include "holberton.h"
 #include <stdlib.h>
 
+/**
+ * create_array - creates an array of chars and initializes it.
+ * @size: size of the array to be created.
+ * @c: char to initialize the array.
+ *
+ * Return: NULL if size is 0. Othwerise returns the pointer to the array,
+ * or NULL if it fails..
+ */
+
 char *create_array(unsigned int size, char c)
 {
 	int asize;
 	char *a;
-	unsigned int print;
+	unsigned int i;
 
 	if (size == 0)
 	{
-		return (0);
+		return (NULL);
 	}
+
 	asize = size - 1;
 	a = malloc(asize * sizeof(char));
-
-	print = 0;
-	while (print < size)
+	if (a == NULL)
 	{
-		a[print] = c;
-		print++;
+		return (NULL);
 	}
 
-	return a;
+	i = 0;
+	while (i < size)
+	{
+		a[i] = c;
+		i++;
+	}
+
+	return (a);
 }
