fe9c8ddd47d926a3130fe6809c0fa60b65e69f83
miguel-dev <miguelacortes@zoho.com>
2019-03-12 01:50:28 +0000

    Task 1

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -10,7 +10,9 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
+	if (name != 0)
+		d->name = name;
 	d->age = age;
-	d->owner = owner;
+	if (owner != 0)
+		d->owner = owner;
 }
