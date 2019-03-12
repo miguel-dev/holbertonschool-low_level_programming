86507e884d9d81d7d64e8b05784bfbc43f90d29d
miguel-dev <miguelacortes@zoho.com>
2019-03-12 01:19:36 +0000

    Task 0

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure defining a dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ *
+ * Description: Struct for dog including name,
+ * age and owner.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
