1e82ef829700d0b55533a4c67262fd93c76f4b6e
miguel-dev <miguelacortes@zoho.com>
2019-03-12 02:41:46 +0000

    Update

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,11 @@
+/**
+ * new_dog - creates a new dog.
+ * @name: name of new dog.
+ * @age: age of new dog.
+ * @owner: owner of new dog.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}
---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,9 @@
+/**
+ * free_dog - frees dogs.
+ * @d: dog to be freed.
+ */
+
+void free_dog(dog_t *d)
+{
+
+}
