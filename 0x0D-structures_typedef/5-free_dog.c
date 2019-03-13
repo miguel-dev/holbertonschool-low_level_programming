c2a05064ac132a95c43b9e84998dc443904e1ae4
miguel-dev <miguelacortes@zoho.com>
2019-03-13 00:15:01 +0000

    Task 5

--- 0x0D-structures_typedef/5-free_dog.c
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -1,9 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+
 /**
  * free_dog - frees dogs.
- * @d: dog to be freed.
+ * @d: dog structure to be freed.
  */
 
 void free_dog(dog_t *d)
 {
-
+	free(d->name);
+	free(d->owner);
+	free(d);
 }
