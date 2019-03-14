1c7f874b712bc8bc3d58a6ed9cf3e40a4d4b78d3
miguel-dev <miguelacortes@zoho.com>
2019-03-14 01:10:36 +0000

    Task 4

--- 0x0E-function_pointers/100-main_opcodes.c
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - prints the opcodes of its own main function.
+ * @argc: number of arguments,
+ * @argv: array of arguments.
+ *
+ * Return: 0 if sucessful.
+ */
+
 int main(int argc, char argv[])
 {
 	int (*p)(int argc, char argv[]);
