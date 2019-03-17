515361c409512c4a8177017f86495f824bd9f3bb
miguel-dev <miguelacortes@zoho.com>
2019-03-17 03:27:38 +0000

    Task 3

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - prints float.
+ * @format: list of types of arguments passed to the function.
+ * @...: parameters.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	char *string;
+	int i, f;
+
+	va_start(list, format);
+	i = 0;
+	while (format[i] && format)
+	{
+		f = 1;
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(list, int));
+				break;
+
+			case 'i':
+				printf("%d", va_arg(list, int));
+				break;
+
+			case 'f':
+				printf("%f", va_arg(list, double));
+				break;
+
+			case 's':
+			{
+				string = va_arg(list, char*);
+				printf("%s", string);
+				break;
+			}
+			default:
+				f = 0;
+		}
+		if (format[i + 1] != '\0' && f != 0)
+			printf(", ");
+
+		i++;
+	}
+	printf("\n");
+	va_end(list);
+}
