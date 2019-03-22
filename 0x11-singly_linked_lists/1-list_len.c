8378de034cd38261b246d522ca0ffddc8ca6aa74
miguel-dev <miguelacortes@zoho.com>
2019-03-22 01:44:15 +0000

    [Task 1]1. List length

---
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list.
+ * @h: pointer to a linked list_t list,
+ *
+ * Return: number of elements in a linked list_t list.
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *iterator;
+	int n = 0;
+
+	iterator = h;
+	while (iterator)
+	{
+		n++;
+		iterator = iterator->next;
+		if (!iterator->next)
+		{
+			n++;
+			break;
+		}
+	}
+	return (n);
+}
