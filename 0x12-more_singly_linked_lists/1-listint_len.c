f3db7d9b63935bdfc651097b73e3ffb743113cf2
miguel-dev <miguelacortes@zoho.com>
2019-03-25 21:59:54 +0000

    [Feauture]1. List length

---
+++ 0x12-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a
+ * linked listint_t list.
+ * @h: pointer to a list.
+ *
+ * Return: number of elements in linked list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t n = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	while (h->next != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+	n++;
+	return (n);
+}
