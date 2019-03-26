a9b283a43899c272aaae0779bed1caa98281b17c
miguel-dev <miguelacortes@zoho.com>
2019-03-26 22:46:19 +0000

    [Fixes] Moved verification of NULL pointer to the top.

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -10,17 +10,15 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int c = 0;
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
 	while (head->next != NULL && c < index)
 	{
 		c++;
 		head = head->next;
 	}
-	if (head == NULL)
-	{
-		return (NULL);
-	}
-	else
-	{
-		return (head);
-	}
+	return (head);
 }
