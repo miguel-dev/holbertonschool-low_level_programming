7b7f497e069d9ac3b528454eeec542a73230a0cc
miguel-dev <miguelacortes@zoho.com>
2019-03-27 02:34:51 +0000

    [Feature]11. Reverse list

---
+++ 0x12-more_singly_linked_lists/100-reverse-listint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverses a listint_t linked list.
+ * @head: address of pointer to list.
+ *
+ * Return: poitner to the first node of reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+
+}
