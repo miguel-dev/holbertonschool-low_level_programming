e2aeda282c8764104cc69e7addfcd48ef821f9e1
miguel-dev <miguelacortes@zoho.com>
2019-03-29 02:41:44 +0000

    [Feature]6. Endianness

--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 
+int _putchar(char);
+
 unsigned int binary_to_uint(const char *b);
 
 void print_binary(unsigned long int n);
@@ -14,5 +16,7 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+int get_endianness(void);
 
 #endif
