05eee44eda3e77e57c7b8dc025a5ebae10688295
miguel-dev <miguelacortes@zoho.com>
2019-09-14 18:29:15 +0000

    [Fixes] Added stdlib

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * add_dnodeint - Adds a new node at the beginning of a dlistint_t list.
+ * @head: Address of head of list.
+ * @n: integer contained in the new node.
+ *
+ * Return: Address of new node or NULL if failed.
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *node;
