1a4fee58ed7b3c15e3d929228af3160f4d46f057
miguel-dev <miguelacortes@zoho.com>
2019-09-16 01:52:43 +0000

    [Feature] Task 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list.
+ * @head: pointer to list.
+ * @index: number of node to be returned.
+ *
+ * Return: node indicated by index or NULL if it doesn't exist.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int c = 0;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	while (head->next != NULL && c < index)
+	{
+		c++;
+		head = head->next;
+	}
+
+	if (c < index)
+	{
+		return (NULL);
+	}
+	return (head);
+}
+
+/**
+ * delete_nodeint_at_index - Deletes the node at index of a listint_t list.
+ * @head: Fist node of list.
+ * @index: Position where node will be deleted.
+ *
+ * Return: 1 if successful, -1 if failed.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *previous, *node;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	previous = get_nodeint_at_index(*head, index - 1);
+	if (previous == NULL || previous->next == NULL)
+		return (-1);
+
+	node = previous->next;
+	previous->next = previous->next->next;
+	node->next = NULL;
+	free(node);
+	return (1);
+}
