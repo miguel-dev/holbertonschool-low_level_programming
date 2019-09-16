8dc471bda03c3dc868199f9a3e19e47b381e7c87
miguel-dev <miguelacortes@zoho.com>
2019-09-16 00:09:37 +0000

    [Fixes] Added definition of function get_dnodeint_at_index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,32 @@
 #include "lists.h"
 
 /**
+ * get_dnodeint_at_index - Returns the nth node of a dlistint_t linked list.
+ * @head: Head of list.
+ * @index: Index where node is located.
+ *
+ * Return: Node at index in list, otherwise return NULL.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int node_count = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (head && node_count < index)
+	{
+		node_count++;
+		head = head->next;
+	}
+
+	if (node_count < index)
+		return (NULL);
+
+	return (head);
+}
+
+/**
  * insert_dnodeint_at_index - inserts a new new_node at a given position.
  * @h: address to pointer of list.
  * @idx: index where the new new_node should be inserted.
