1a7d8f5b158feeee590c97ddc2723e12ad9c2dd7
miguel-dev <miguelacortes@zoho.com>
2019-09-22 01:05:48 +0000

    [Fixes] Checks if the following node exists

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -54,9 +54,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (-1);
 
 	node = previous->next;
-	previous->next = previous->next->next;
-	if (previous->next)
+	if (previous->next->next)
+	{
+		previous->next = previous->next->next;
 		previous->next->prev = previous;
+	}
 	node->next = NULL;
 	free(node);
 	return (1);
