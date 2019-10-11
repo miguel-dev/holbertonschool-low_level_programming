ccfa5b3ab6a67dae168b62f33eeb42f6f1f06476
miguel-dev <miguelacortes@zoho.com>
2019-10-11 21:49:45 +0000

    [Fixes] strcmp tp compare the keys to find a match

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,7 +27,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	l = ht->array[index];
 	while (l)
 	{
-		if (l->key == key)
+		if (strcmp(l->key, key) == 0)
 		{
 			l->value = (char *)value;
 			return (1);
