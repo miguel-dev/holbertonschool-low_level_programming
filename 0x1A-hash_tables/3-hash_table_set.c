03342969931ee771dc50d3662827b0c22150d22d
miguel-dev <miguelacortes@zoho.com>
2019-10-11 23:08:42 +0000

    [Fixes] Checks if key is empty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *l, *new_node;
 
-	if (!ht || !key || !value)
+	if (!ht || !key || !strcmp(key, "") || !value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
