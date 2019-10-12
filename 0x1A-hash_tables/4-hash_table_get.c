9ff2f124475ba6ebbfa07a4f9897a07cf2d05714
miguel-dev <miguelacortes@zoho.com>
2019-10-12 00:04:34 +0000

    [Feature] Task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key.
+ * @ht: Hash table
+ * @key: key we are finding a match.
+ *
+ * Return: Value associated with the key, or NULL if it can't be found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			return (node->value);
+		}
+		node = node->next;
+	}
+	return (NULL);
+}
