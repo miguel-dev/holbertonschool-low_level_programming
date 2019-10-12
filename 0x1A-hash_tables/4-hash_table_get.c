b3a5b9fe5a34a9b8ee200c025fb3903f2135706e
miguel-dev <miguelacortes@zoho.com>
2019-10-12 00:24:24 +0000

    [Fixes] Added conditions at the beginning

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,8 +12,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *node;
 
+	if (!ht || !key || !*key)
+		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
+
 	while (node)
 	{
 		if (strcmp(node->key, key) == 0)
