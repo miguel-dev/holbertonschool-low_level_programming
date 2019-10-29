2dea8422af359d5f061b4867654f52b4188883d3
miguel-dev <miguelacortes@zoho.com>
2019-10-29 23:58:28 +0000

    [Features] Task 2. Selection sort

---
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts an array of integers in ascending order.
+ * @array: Array to be sorted.
+ * @size: Size of array.
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, min, b;
+	int temp;
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (b = i + 1; b < size; b++)
+		{
+			if (array[b] < array[min])
+			{
+				min = b;
+			}
+		}
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
+}
