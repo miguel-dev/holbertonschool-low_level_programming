5ea882759ec51ca73527f92ff31bbb98abb6c1be
miguel-dev <miguelacortes@zoho.com>
2020-04-17 02:24:53 +0000

    [Feature] 0. Linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+/**
+ * linear_search - Checks each element of the array until a match is found
+ * or all elements are compared.
+ * @array: Pointer to the first element of the array.
+ * @size: Number of elements in the array.
+ * @value: Value we are searching for.
+ *
+ * Return: Index where value is located. If the pointer to the array is NULL
+ * or value is not present returns -1.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
