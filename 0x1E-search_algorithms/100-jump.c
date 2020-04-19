93833387dcc4ba0a73dfba7f1c6d296c6730ed5a
miguel-dev <miguelacortes@zoho.com>
2020-04-19 20:34:53 +0000

    [Fixes] Checks if next is equal to size of array

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -30,7 +30,7 @@ int jump_search(int *array, size_t size, int value)
 	printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
 	printf("Value found between indexes [%lu] and [%lu]\n", prev, next);
 
-	if (next > size)
+	if (next >= size)
 	{
 		next = size - 1;
 	}
