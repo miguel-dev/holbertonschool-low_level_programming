0a28270d8882ef9d457c1aa994e4be9a70e7e7a8
miguel-dev <miguelacortes@zoho.com>
2020-04-23 17:26:20 +0000

    [Fixes] It first checks if the value is less than size to avoid getting out of bounds

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -20,7 +20,7 @@ int jump_search(int *array, size_t size, int value)
 	if (!array)
 		return (-1);
 
-	while (array[next] < value && next < size)
+	while (next < size && array[next] < value)
 	{
 		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
 		prev = next;
